6b5f6ce7e72460eb0e56941db343fe58606baee1
Uchio KONDO <udzura@pepabo.com>
2014-10-03 19:03:56 +0900

    Work `cap production provision' uniquitously

--- spec/misc/config/deploy/production.rb
+++ spec/misc/config/deploy/production.rb
@@ -1,12 +1,24 @@
+if `which boot2docker`.empty?
+  hostname = 'localhost'
+else
+  hostname = `boot2docker ip 2>/dev/null`.chomp
+end
+hostname = ENV['HOSTNAME'] if ENV['HOSTNAME']
+
+port_string = `docker port test_sshd 22`
+port = port_string.split(':')[1].to_i unless port_string.empty?
+
 set :puppet_roles, :app
 set :puppet_modulepath, "modules"
 set :puppet_manifest_file, "manifests/site.pp"
 
-server '192.168.59.103',
+puts "%s:%d" % [hostname, port]
+
+server hostname,
   roles: %w{app},
   ssh_options: {
-    port: 49161,
-    user: 'root', # overrides user setting above
+    port: port,
+    user: 'root',
     keys: %w(./id_dsa),
     forward_agent: false,
     auth_methods: %w(publickey)
